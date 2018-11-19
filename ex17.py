5bb972a0edc8ef356839d145997d260387ab28d4
Paul De Palma <depalma@gonzaga.edu>
2018-11-19 14:14:58 -0800

    sound file

--- ex17.py
+++ ex17.py
@@ -13,6 +13,13 @@ def main():
     reader = csv.reader(open(fin, 'rb'), delimiter= ',')
     x = list(reader)
     result = numpy.array(x).astype('float')
+    print fin
+    print sys.argv[2]
+    print sys.argv[3]
+    stuff = sys.argv[3]
+    print stuff[0]
+    print stuff[1]
+    print stuff[2]
     print result    #print the array
     print result[0]  #print the 0th row
     print result[1][3]  #print the cell in the 1st row 3rd col, .01
